fa57e1e98ef70da10c26527aa8c90ebde2719b26
ekourtakis <emmanuel.kourtakis@temple.edu>
2024-09-30 15:28:43 -0400

    refactored getView() as scope function

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -33,27 +33,21 @@ class MainActivity : AppCompatActivity() {
 
     // Return true if average value in list is greater than median value, false otherwise
     private fun averageLessThanMedian(listOfNumbers: List<Double>)
-        = listOfNumbers.average() < if (listOfNumbers.size % 2 == 0)
-                                        (listOfNumbers.sorted()[listOfNumbers.size / 2] + listOfNumbers.sorted()[(listOfNumbers.size - 1) / 2]) / 2
-                                    else
-                                        listOfNumbers.sorted()[listOfNumbers.size / 2]
+        = listOfNumbers.average() <
+            if (listOfNumbers.size % 2 == 0)
+                    (listOfNumbers.sorted()[listOfNumbers.size / 2] + listOfNumbers.sorted()[(listOfNumbers.size - 1) / 2]) / 2
+            else
+                listOfNumbers.sorted()[listOfNumbers.size / 2]
 
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
-
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
-        }
-
-        textView.text = collection[position].toString()
-
-        return textView
-    }
+    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context) =
+        (if (recycledView != null)
+            (recycledView as TextView)
+        else
+            TextView(context).apply {
+                setPadding(5,10,10,0)
+                textSize = 22f
+            }).apply { text = collection[position].toString() }
 
 }
